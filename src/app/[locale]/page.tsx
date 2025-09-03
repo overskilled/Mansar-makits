1b789608424c30b04b512c17c9be7edd66eb88c9
ing-andy <lefilsdecathy@gmail.com>
2025-09-03 12:54:59 +0100

    home page 95%

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import { MoveUpRight } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
-  const dataBlock = [
+  const dataBlock1 = [
     {
       image:
         "https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…c22e_user-profile-change-refresh.svg?fit=max&w=96",
@@ -32,6 +32,50 @@ export default function Home() {
       title: "Regulatory coverage",
       describ:
         "Our team, combining deep brokerage and regulatory knowledge, works closely with you, allowing you to scale efficiently without regulatory overhead slowing you down.",
+    },
+  ];
+  const dataBlock2 = [
+    {
+      image:
+        "https://lemonmarkets2.cdn.prismic.io/lemonmarkets2/dc7ae402-3d09-473d-a8bf-dc32f154d22c_user-document.svg?fit=max&w=96",
+      title: "Account opening & management",
+      describ:
+        "Create user accounts in seconds, including all identification, verification, and compliance checks relevant & needed.",
+    },
+    {
+      image:
+        "		https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…b386e1f9_money-banknote-exchange.svg?fit=max&w=96",
+      title: "Account funding & withdrawing",
+      describ:
+        "Allow your customers to easily top-up and withdraw funds from their cash account",
+    },
+    {
+      image:
+        "	https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…ing-code-document-list-checkmark.svg?fit=max&w=96",
+      title: "Order management & execution",
+      describ:
+        "Access a wide range of ETFs, Funds, and Stocks, through our state-of-the-art order management system.",
+    },
+    {
+      image:
+        "	https://lemonmarkets2.cdn.prismic.io/lemonmarkets2/eb140aa0-1bb2-4e5b-8b2e-41caefdd9d5a_table-edit-chart.svg?fit=max&w=96",
+      title: "Custody & asset servicing",
+      describ:
+        "Entrust us to handle the safe-keeping and servicing of all your customers' assets.",
+    },
+    {
+      image:
+        "https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…dfc0a2ce32_documents-list-shield.svg?fit=max&w=96",
+      title: "Taxes, reporting & receipts",
+      describ:
+        "Let us take over all administrative tasks that come along with investing, from creating tax statements to order receipts.",
+    },
+    {
+      image:
+        "https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…programming-code-window-terminal.svg?fit=max&w=96",
+      title: "Self-Service Customer Portal",
+      describ:
+        "Manage customer operations, requests and reporting easily - all in one convenient, self-service dashboard.",
     },
   ];
   return (
@@ -161,9 +205,12 @@ export default function Home() {
             />
           </div>
           <div className="w-4/6 flex flex-wrap gap-10">
-            {dataBlock.map((el, index) => (
-              <Card className=" border shadow-lg p-1 rounded-md w-[45%]" key={index}>
-                <Card className="-ml-4 -mb-4 rounded-md h-70">
+            {dataBlock1.map((el, index) => (
+              <Card
+                className=" border shadow-lg p-1 rounded-md w-[45%]"
+                key={index}
+              >
+                <Card className="-ml-4 -mb-4 rounded-md h-75">
                   <CardHeader className="gap-2">
                     <img src={el.image} alt="image" />
                     <h3 className="text-3xl">{el.title}</h3>
@@ -173,6 +220,134 @@ export default function Home() {
               </Card>
             ))}
           </div>
+        </div>
+      </div>
+      <div className="flex bg-black w-full py-10 items-center my-10">
+        <div className="flex flex-col h-80 w-full">
+          <span className="bg-blue-600/20 px-3 py-1 w-35 text-indigo-800">
+            Cooperations
+          </span>
+          <h3 className="text-4xl text-white mt-5 mb-20">
+            Working together with trusted institutions
+          </h3>
+          <div className="w-[95%] ml-2 bg-gray-300 h-30 ">d</div>
+        </div>
+      </div>
+      <div className="flex w-full flex-col">
+        <span className="py-1 px-2 bg-blue-600/20 rounded-2xl w-40 text-center">
+          Operating Model
+        </span>
+        <h3 className="text-4xl font-semibold text-shadow-xl my-10">
+          A powerful infrastructure compatible with any operating model
+        </h3>
+        <p className="text-xl">
+          We recognise that every organisation is unique, with its own distinct
+          goals and setups. That’s why we’ve developed a platform that adapts to
+          how you operate, empowering you to build exactly what you want.
+        </p>
+        <div className="flex flex-col mt-10 mb-30">
+          <div className="flex">
+            <div className="flex h-90 flex-col gap-10 w-3/7">
+              <p className="text-center">Select the right product setup</p>
+              <Card className=" border shadow-lg p-1 bg-gray-100 duration-500 hover:bg-gray-200 rounded-md w-[45%">
+                <Card className="-ml-6 -mb-6 rounded-md h-45 bg-gray-100 duration-500 hover:bg-amber-100">
+                  <CardHeader className="m-0">
+                    <h3 className="text-md font-semibold">bundled</h3>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-sm">
+                      Build your investment product end-to-end with
+                      lemon.markets. We handle all regulatory, technical, and
+                      operational complexities, providing both securities and
+                      cash accounts, so you can focus on building the
+                      best-in-class investment solution for your customers.
+                    </p>
+                  </CardContent>
+                </Card>
+              </Card>
+              <Card className=" border shadow-lg p-1 bg-gray-100 hover:bg-bg-gray-200 duration-500 rounded-md w-[45%">
+                <Card className="-ml-6 -mb-6 rounded-md h-45 bg-gray-100 hover:bg-amber-100 duration-500">
+                  <CardHeader className="">
+                    <h3 className="text-md font-semibold">Unbundled</h3>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-sm">
+                      Build your investment product end-to-end with
+                      lemon.markets. We handle all regulatory, technical, and
+                      operational complexities, providing both securities and
+                      cash accounts, so you can focus on building the
+                      best-in-class investment solution for your customers.
+                    </p>
+                  </CardContent>
+                </Card>
+              </Card>
+            </div>
+            <div className="flex justify-end w-4/7">
+              <img
+                src="https://lemonmarkets2.cdn.prismic.io/lemonmarkets2/Z3KHhpbqstJ984vu_bundledoperatingmodel.svg?fit=max&w=1200"
+                alt="image"
+                className="right-0"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col my-20 w-full ">
+        <span className="py-1 px-2 bg-blue-600/20 rounded-2xl w-40 text-center"> Functionality </span>
+        <h3 className="text-4xl font-semibold text-shadow-xl my-10">
+          The full value chain of investing at your fingertips
+        </h3>
+        <p>Every functionality our infrastructure offers is thought through from the customer's point of view. This enables you to exceed expectations from day one, securing a strong competitive advantage. We envision a future where investing is omnipresent. Therefore, we are delivering on a clear product vision to truly democratize investing.</p>
+        <div className="flex flex-wrap justify-between my-20">
+          {dataBlock2.map((el, index) => (
+            <Card
+            className=" border-none borders shadow-none p-2 rounded-md my-4 w-[32%]"
+            key={index}
+            >
+              <Card className="-ml-4 -mb-4 rounded-md h-85 hover:bg-gray-100 shadow-none border-none borders bg-white  justify-between gap-0 duration-200">
+                <CardHeader className="gap-2">
+                  <img src={el.image} alt="image" />
+                  <h3 className="text-3xl mt-4">{el.title}</h3>
+                </CardHeader>
+                <CardContent>
+                  {el.describ}
+                </CardContent>
+                <CardFooter className="bottom-0 ">
+                  <span className="flex gap-4 group hover:text-amber-400 duration-500">learn more <MoveUpRight className="w-12 duration-500 group-hover:w-14" /></span>
+                </CardFooter>
+              </Card>
+            </Card>
+          ))}
+        </div>
+      </div>
+      <div className="w-full flex *:w-1/2 bg-black linear h100 pt-30 mb-20 ">
+        <div className="flex flex-col pl-4">
+          <span className="py-1 px-2 bg-blue-600/20 text-indigo-700 rounded-2xl w-40 text-center"> Get in Touch </span>
+          <h1 className="text-6xl text-white mt-5"> Partner with us </h1>
+          <p className="text-gray-200 mb-10 mt-5">Use the lemon.markets API to launch a customer-centric and compliant investment product.</p>
+          <button className="py-3 px-6 rounded-lg bg-white w-50">start building</button>
+        </div>
+        <div className="flex flex-col justify-end items-center border pl-10">
+          <Card
+            className=" border-none borders shadow-none w-full p-0 bg-transparent hover:bg-black rounded-md my-4 "
+            >
+              <Card className="mt-4 -ml-4 rounded-md w-full hover:bg-black bg-transparent shadow-none border-none borders justify-between gap-0 duration-200">
+                <CardHeader className="gap-2">
+                  <h3 className="text-3xl mt-4 text-white">About us</h3>
+                </CardHeader>
+                <CardContent className="text-gray-500">discover the vision and principles of MANSAR.MAKITS</CardContent>
+              </Card>
+            </Card>
+            <Card
+            className=" border-none borders shadow-none w-full hover:bg-black bg-transparent p-0 rounded-md my-4 "
+            >
+              <Card className="-ml-4 -mb-4 rounded-md w-full hover:bg-black bg-transparent shadow-none border-none borders  justify-between gap-0 duration-200">
+                <CardHeader className="gap-2">
+                  <h3 className="text-3xl mt-4 text-white">Carrer <MoveUpRight className="hidden hover:flex"/></h3>
+                </CardHeader>
+                <CardContent className="text-gray-500">unclock your full potential and grow with us </CardContent>
+              </Card>
+            </Card>
         </div>
       </div>
     </div>
