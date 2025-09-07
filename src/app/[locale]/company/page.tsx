a5b943f1f8b9697a43e5b4c8ceb18922fbc6003b
ing-andy <lefilsdecathy@gmail.com>
2025-09-07 16:57:19 +0100

    update

--- src/app/[locale]/company/page.tsx
+++ src/app/[locale]/company/page.tsx
@@ -1,3 +1,6 @@
+import Entete from "@/components/custom/Entete";
+import Footer from "@/components/custom/Footer";
+import GetInTouchBlock from "@/components/custom/GetInTouchBlock";
 import Hero from "@/components/custom/Hero";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import {
@@ -7,7 +10,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { MoveUpRight } from "lucide-react";
+import { MoveDownRight, MoveUpRight } from "lucide-react";
 
 export default function page() {
   const heroData = {
@@ -17,33 +20,27 @@ export default function page() {
   };
   const ListeDOubleData = [
     {
-      image:
-        "https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…0-f3d70e39a8d7_customer+first+1.svg?fit=max&w=256",
+      image: "/andy.jpg",
       text: "Put the customer first",
     },
     {
-      image:
-        "https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…24-f64fee27b87a_mission+focus+1.svg?fit=max&w=256",
+      image: "/andy.jpg",
       text: "Grow investing opportunities",
     },
     {
-      image:
-        "https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…f7-486f5949f413_working+style+1.svg?fit=max&w=256",
+      image: "/andy.jpg",
       text: "Deliver high quality outcomes",
     },
     {
-      image:
-        "https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…2a2e8962fbb0_leadership+style+1.svg?fit=max&w=256",
+      image: "/andy.jpg",
       text: "Lead with Empathy",
     },
     {
-      image:
-        "https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…ab6-2cd6ca55dd06_relationship+1.svg?fit=max&w=256",
+      image: "/andy.jpg",
       text: "Be a team player",
     },
     {
-      image:
-        "https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…-71101816bbfb_personal+growth+1.svg?fit=max&w=256",
+      image: "/andy.jpg",
       text: "Accept no failure, only feedback",
     },
   ];
@@ -74,86 +71,93 @@ export default function page() {
       role: "responsable frontend",
     },
   ];
-  const listeImage = [
-    "https://images.prismic.io/lemonmarkets2/4567b528-4…ffice+2.png?auto=compress%2Cformat&fit=max&w=3840",
-    "https://images.prismic.io/lemonmarkets2/1ee65d05-5…ffice+3.png?auto=compress%2Cformat&fit=max&w=3840",
-    "https://images.prismic.io/lemonmarkets2/365d9ad9-2…ffice+4.png?auto=compress%2Cformat&fit=max&w=3840",
-  ];
+  const listeImage = ["/andy.jpg", "/andy.jpg", "/andy.jpg"];
+  const EnteteData = {
+    SPAN: "The latest coverage",
+    H3: "Our story has been covered by media outlets all across Europe and beyond",
+    P: "",
+  };
   return (
     <div className="flex flex-col min-h-screen">
       <Hero H2={heroData.H2} P={heroData.P} B={heroData.B} />
 
       <div className="w-full my-10">
-        <div className="w-full flex my-10">
+        <div className="w-full flex my-15 max-h-80 ">
           <div className="float-left w-1/2">
-            <span className="px-4 py-0.5 bg-indigo-100 text-shadow-md text-indigo-500 rounded-3xl">
-              What our vision is
-            </span>
-            <h3 className="my-4 text-3xl text-shadow-lg">
-              Enabling anyone to be an investor
-            </h3>
-            <p>
-              We envision a future in which anyone is an investor. Therefore, we
-              provide the infrastructure for the next generation of innovative
-              companies that help their customers build wealth. Working our way
-              towards our mission: to grow investing opportunities for all
-              European customers and businesses, providing seamless access to
-              capital markets.
-            </p>
+            <div className="h-full pr-30">
+              <span className="px-4 py-0.5 bg-indigo-100 text-shadow-md text-indigo-500 rounded-3xl">
+                What our vision is
+              </span>
+              <h3 className="my-8 text-3xl text-shadow-lg">
+                Enabling anyone to be an investor
+              </h3>
+              <p>
+                We envision a future in which anyone is an investor. Therefore,
+                we provide the infrastructure for the next generation of
+                innovative companies that help their customers build wealth.
+                Working our way towards our mission: to grow investing
+                opportunities for all European customers and businesses,
+                providing seamless access to capital markets.
+              </p>
+            </div>
           </div>
           <div className="float-right w-1/2 flex justify-end ">
             <img
-              src="https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…070e9ac2_vision+every+european.svg?fit=max&w=1080"
+              src="/andy.jpg"
               alt="yess"
-              className="m-2 bg-gray-50"
+              className="mx-2 bg-gray-50 w-full h-full object-cover rounded-lg"
             />
           </div>
         </div>
-        <div className="w-full flex my-15">
-          <div className="float-right w-1/2 flex justify-start">
+        <div className="w-full flex my-15 max-h-80 ">
+          <div className="float-right w-1/2 pr-4 flex justify-start">
             <img
-              src="https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…b280-0679816b3b78_why+we+do+it.svg?fit=max&w=1080"
+              src="/andy.jpg"
               alt="yess"
-              className="m-2 bg-gray-50"
+              className="mx-2 bg-gray-50 w-full h-full object-cover rounded-lg"
             />
           </div>
-          <div className="float-left w-1/2 my-15">
-            <span className="px-4 py-0.5 bg-indigo-100 text-shadow-md text-indigo-500 rounded-3xl">
-              Why we do it
-            </span>
-            <h3 className="my-4 text-3xl text-shadow-lg">
-              Because equity rails are still full of manual processes and legacy
-              technology
-            </h3>
-            <p>
-              We experienced ourselves how much legacy technology, manual
-              processes and being left alone with all regulatory complexity can
-              get in the way of building great investment products. This
-              shouldn’t happen to anyone after us.
-            </p>
+          <div className="float-left w-1/2 flex flex-col items-center pl-30">
+            <div className="h-full">
+              <span className="px-4 py-0.5 bg-indigo-100 text-shadow-md text-indigo-500 rounded-3xl">
+                Why we do it
+              </span>
+              <h3 className="my-8 text-3xl text-shadow-lg">
+                Because equity rails are still full of manual processes and
+                legacy technology
+              </h3>
+              <p>
+                We experienced ourselves how much legacy technology, manual
+                processes and being left alone with all regulatory complexity
+                can get in the way of building great investment products. This
+                shouldn’t happen to anyone after us.
+              </p>
+            </div>
           </div>
         </div>
-        <div className="w-full flex my-15">
-          <div className="float-left w-1/2">
-            <span className="px-4 py-0.5 bg-indigo-100 text-shadow-md text-indigo-500 rounded-3xl">
-              How we do it
-            </span>
-            <h3 className="my-4 text-3xl text-shadow-lg">
-              We offer partners a one-stop-shop to launch their investment
-              product in weeks
-            </h3>
-            <p>
-              investment products. Our one-stop-shop approach abstracts all
-              technical complexity via our API, provides regulatory coverage
-              through our BaFin licenses and enables smooth operations guided by
-              our experts.
-            </p>
+        <div className="w-full flex my-15 max-h-80">
+          <div className="float-left w-1/2 flex flex-col items-center pr-30">
+            <div className="h-full">
+              <span className="px-4 py-0.5 bg-indigo-100 text-shadow-md text-indigo-500 rounded-3xl">
+                How we do it
+              </span>
+              <h3 className="my-8 text-3xl text-shadow-lg">
+                We offer partners a one-stop-shop to launch their investment
+                product in weeks
+              </h3>
+              <p>
+                investment products. Our one-stop-shop approach abstracts all
+                technical complexity via our API, provides regulatory coverage
+                through our BaFin licenses and enables smooth operations guided
+                by our experts.
+              </p>
+            </div>
           </div>
           <div className="float-right w-1/2 flex justify-end ">
             <img
-              src="https://lemonmarkets2.cdn.prismic.io/lemonmarkets2…bf0c-8a49ca68ec4b_how+we+do+it.svg?fit=max&w=1080"
+              src="/andy.jpg"
               alt="yess"
-              className="m-2 bg-gray-50"
+              className="mx-2 bg-gray-50 w-full h-full object-cover rounded-lg"
             />
           </div>
         </div>
@@ -186,14 +190,14 @@ export default function page() {
               className="shadow-none p-0 min-w100 w-[32%] rounded-md pr-2 -pt-4 mb-5"
             >
               <Card className="-ml-4 mt-2 shadow-none p-0 rounded-md -mb-4 gap-0">
-                <CardContent className="w-full h-40 justify-center">
+                <CardContent className="w-full h-40 flex mb-2 justify-center pt-2">
                   <img
                     src={el.image}
                     alt="image"
-                    className="object-cover w-full h-full"
+                    className="object-cover w-40 h-full"
                   />
                 </CardContent>
-                <CardFooter className="justify-center mb-5">
+                <CardFooter className="justify-center mb-2">
                   {el.text}
                 </CardFooter>
               </Card>
@@ -278,12 +282,129 @@ export default function page() {
             <img
               src={el}
               alt="image"
-              className="basis-3 rounded-2xl shadow"
+              className={`w-1/3 h-70 object-cover rounded-2xl ${
+                index == 1 ? "mx-5" : ""
+              } shadow`}
               key={index}
             />
           ))}
         </div>
       </div>
+
+      <div className="w-full bg-black h-70 flex flex-col justify-center my-10">
+        <div className="w-full">
+          <h3 className="text-4xl mb-5 text-white">Our backing</h3>
+          <p className="text-gray-600">
+            We are excited to be entering a second phase in our growth, having
+            joined forces with dwpbank. While we are undergoing our transition,
+            you can read more on the latest news in our Media section.
+          </p>
+        </div>
+      </div>
+      <div className="w-full">
+        <Entete
+          SPAN={EnteteData.SPAN}
+          H3={EnteteData.H3}
+          P={EnteteData.P}
+          TEXT="text-indigo-600"
+          BG="bg-indigo-300"
+        />
+
+        <div className="bg-gray-100 w-1/2 border rounded-2xl items-center p-4 flex justify-around *:w-1/3 *:hover:bg-white *:py-2 *:rounded-md *:text-center">
+          <p>international publication</p>
+          <p>german publication</p>
+          <p>press relanses</p>
+        </div>
+
+        <div className="flex flex-col w-full mt-10">
+          <div className="w-full *:py-4 *:my-5 *:flex">
+            <div className="w-full">
+              <span className="w-1/5">07-aout-2025</span>
+              <div className="w-4/5">
+                <p>dsanj jdsanln jsdanlndls alnlks dkla ska dal</p>
+                <span className="px-3 py-0.5 bg-gray-400 rounded-2xl my-4">
+                  article
+                </span>
+              </div>
+            </div>
+            <div className="w-full">
+              <span className="w-1/5">07-aout-20</span>
+              <div className="w-4/5">
+                <p className="mb-4 text-indigo-500 hover:text-gray-600">
+                  dsanj jdsanln jsdanlndls alnlks dkla ska dal
+                </p>
+                <span className="px-3 py-0.5 bg-gray-400 rounded-2xl">
+                  article
+                </span>
+              </div>
+            </div>
+            <div className="w-full">
+              <span className="w-1/5">07-aout-2025</span>
+              <div className="w-4/5">
+                <p className="mb-4 text-indigo-500 hover:text-gray-600">
+                  dsanj jdsanln jsdanlndls alnlks dkla ska dal
+                </p>
+                <span className="px-3 py-0.5 bg-gray-400 rounded-2xl">
+                  article
+                </span>
+              </div>
+            </div>
+            <div className="w-full">
+              <span className="w-1/5">07-aout-2025</span>
+              <div className="w-4/5">
+                <p className="mb-4 text-indigo-500 hover:text-gray-600">
+                  dsanj jdsanln jsdanlndls alnlks dkla ska dal
+                </p>
+                <span className="px-3 py-0.5 bg-gray-400 rounded-2xl">
+                  article
+                </span>
+              </div>
+            </div>
+            <div className="w-full">
+              <span className="w-1/5">07-aout-2025</span>
+              <div className="w-4/5">
+                <p className="mb-4 text-indigo-500 hover:text-gray-600">
+                  dsanj jdsanln jsdanlndls alnlks dkla ska dal
+                </p>
+                <span className="px-3 py-0.5 bg-gray-400 rounded-2xl">
+                  article
+                </span>
+              </div>
+            </div>
+            <div className="w-full">
+              <span className="w-1/5">07-aout-2025</span>
+              <div className="w-4/5">
+                <p className="mb-4 text-indigo-500 hover:text-gray-600">
+                  dsanj jdsanln jsdanlndls alnlks dkla ska dal
+                </p>
+                <span className="px-3 py-0.5 bg-gray-400 rounded-2xl">
+                  article
+                </span>
+              </div>
+            </div>
+            <div className="w-full">
+              <span className="w-1/5">07-aout-2025</span>
+              <div className="w-4/5">
+                <p className="mb-4 text-indigo-500 hover:text-gray-600">
+                  dsanj jdsanln jsdanlndls alnlks dkla ska dal
+                </p>
+                <span className="px-3 py-0.5 bg-gray-400 rounded-2xl">
+                  article
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <span className="hover:underline flex gap-4 text-lg mx-65 cursor-pointer my-10">
+          {" "}
+          read more <MoveDownRight className="w-5" />
+        </span>
+      </div>
+
+      <GetInTouchBlock />
+
+      <Footer />
     </div>
   );
 }
