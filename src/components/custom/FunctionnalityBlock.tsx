477b34a5e5620b7475d411748ed06bfd77fac673
overskilled <ouateedemloic@gmail.com>
2025-09-28 01:28:25 +0100

    Checkpoint

--- src/components/custom/FunctionnalityBlock.tsx
+++ src/components/custom/FunctionnalityBlock.tsx
@@ -21,10 +21,12 @@ export default function FunctionnalityBlock({ SPAN, H3, P, BLOCK }: Liste) {
   return (
     <div className="px-8 md:px-0 mb-12">
       {/* Header */}
-      <Entete SPAN={SPAN} H3={H3} P={P} TEXT="text-[#6583d2]" BG="bg-[#e9e7db]" />
+      <div className="max-w-7xl mx-auto px-8 md:px-0">
+        <Entete SPAN={SPAN} H3={H3} P={P} TEXT="text-[#6583d2]" BG="bg-[#e9e7db]" />
+      </div>
 
       {/* Cards container */}
-      <div className="max-w-7xl mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 my-20">
+      <div className="max-w-7xl mx-auto px-8 md-px-0 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 my-20">
         {BLOCK.map((el, index) => (
           <Card key={index} className="border shadow-lg p-2 rounded-md w-full">
             <Card className="-ml-2 md:-ml-4 hover:cursor-pointer -mb-5 pb-4 gap-2 duration-300 rounded-md h-72 md:h-80 hover:bg-gray-100">
