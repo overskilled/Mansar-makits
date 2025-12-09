686741303efd4837aeb31fd38d04a2595becda72
overskilled <ouateedemloic@gmail.com>
2025-12-09 23:57:00 +0100

    Finished

--- src/proxy.ts
+++ src/proxy.ts
@@ -9,7 +9,7 @@ const I18nMiddleware = createI18nMiddleware({
 
 
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
     return I18nMiddleware(request)
 }
 
